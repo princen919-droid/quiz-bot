f7638ef46ddefc055daa88cd74a05b6f1a23b9a8
princen <prin.cen919@gmail.com>
2026-04-25 11:58:03 +0530

    new ai version changed

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash"
+  model: "gemini-1.5-flash-latest"
 });
 
 const bot = new Telegraf(BOT_TOKEN);
